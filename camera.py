0241f128ffc2959fd2e1a563eec091ff2c6e312a
Alana <alana.ingrid@ufpe.br>
2021-12-14 19:19:14 -0300

    chega  CHEGA

--- camera.py
+++ camera.py
@@ -12,38 +12,38 @@ class myCamera():
         print('init camera')
         # Bridge to convert ROS message to openCV
         self.bridge = CvBridge()
-
+        self.resposta = ''
         # Subscriber to the camera image
         self.image_sub = rospy.Subscriber("/xtion/rgb/image_color",Image,self.callback_SubscribeCamera)
 
         # Server Service camera
 
 
-
-
     def callback_ServiceCamera(self, request):
         print('executing addImage service')
         request = self.cv_image
         (blueChannel, greenChannel, redChannel) = cv2.split(request)
-        cv2.imshow("Green", greenChannel)
-        ret, t = cv2.threshold(greenChannel, 160, 255, cv2.THRESH_BINARY)
+        #cv2.imshow("Green", greenChannel)
+        ret, t = cv2.threshold(greenChannel, 50, 255, cv2.THRESH_BINARY)
         blackpx = 0
-        whitepx = 0
         row = len(t)
         column = len(t[0])
-        cv2.imshow("thresh", t)
-        cv2.waitKey(0)
+       # cv2.imshow("thresh", t)
+       # cv2.waitKey(0)
         for i in range(0, row):
             for j in range(0, column):
                 px = t[i][j]
                 if px == 0:
-                    blackpx = blackpx + 1 #pixels verdes
-                elif px == 255:
-                    whitepx = whitepx + 1 #pixels vermelhos
-        if whitepx > blackpx:
-            return addImageResponse('green')
-        elif blackpx >  whitepx:
-            return addImageResponse('red')
+                    blackpx = blackpx + 1
+                    print('blackpx: {}'.format(blackpx))
+        if blackpx > 100:
+            self.resposta = addImageResponse('red')
+            print(self.resposta)
+            return self.resposta
+        else:
+            self.resposta = addImageResponse('green')
+            print(self.resposta)
+            return self.resposta
 
 
     def callback_SubscribeCamera(self, msg):
@@ -58,9 +58,9 @@ class myCamera():
             print(e)
 
         # cv_image[linha][coluna][bgr] bgr-> 0:blue, 1:green, 2:red
-        print(self.cv_image[0][0])
-        print(self.cv_image[0][0][0])
+        #print(self.cv_image[0][0])
+        #print(self.cv_image[0][0][0])
 
         # Display the image
-        cv2.imshow("raw", self.cv_image)
-        cv2.waitKey(3)
+        #cv2.imshow("raw", self.cv_image)
+        #cv2.waitKey(3)
--- main.py
+++ main.py
@@ -1,74 +1,145 @@
-#! shebang
+#! /usr/bin/python
 
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
+from sensor_msgs.msg import LaserScan
+from programacao.srv import addImage, addImageResponse
+from sensor_msgs.msg import Image
 import math
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from camera import myCamera
 
 class myRobot():
 
     def __init__(self):
         print('init')
         # Subscriber odometria
-        self.odometria_sub = rospy.Subscriber('/mobile_base_controller/odom', Odometry, self.callback_odometria(), queue_size=1)
+        self.sub_odom = rospy.Subscriber('/mobile_base_controller/odom', Odometry, self.callback_odom, queue_size=1)
         self.row = 0
+        self.midview = 0
+        self.leftview = 0
+        self.rightview = 0
         self.pitch = 0
         self.yaw = 0
-        self.kP = 0.5
+        self.kP = 1 #controlador arbitrario
         # Subscriber laser
+        self.sub_laser = rospy.Subscriber('/scan_raw', LaserScan, self.callback_laser, queue_size=1)
         # Client Service camera
+
         # Publisher base
-        self.base_pub = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=1)
+        self.pub_cmd = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=1)
         self.cmd = Twist()
         # Publisher cabeca
+        self.pub_head = rospy.Publisher('/head_controller/command', JointTrajectory, queue_size=1)
+        self.cmd_head = JointTrajectory()
+        self.points = JointTrajectoryPoint()
 
-    def callback_odometria(self, msg):
+    def callback_odom(self, msg):
         print('callback odometria')
         # Armazenar os dados de odometria
-        self.orientation = msg.pose.pose.orientation
-        self.orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
-        (self.row, self.pitch, self.yaw) = euler_from_quaternion(orientation_list)
+        self.orientation = msg.pose.pose.orientation #posicao quaternion
+        self.orientation_list = [self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w]
+        (self.row, self.pitch, self.yaw) = euler_from_quaternion(self.orientation_list)
+        print(self.yaw)
 
     def callback_laser(self, msg):
-        print('callback laser')
-        # Armazenar os dados do laser
+        self.midview = msg.ranges[333] #sensor do ranges central
+        leftdata = 0
+        rightdata = 0
+        for i in range(0,83):
+            rightdata = rightdata + msg.ranges[i]
+        for k in range(583,666):
+            leftdata = leftdata + msg.ranges[k]
+        self.leftview = leftdata/83 #faixa considerada como visao esquerda
+        self.rightview = rightdata/83 #faixa considerada como visao direita
+       # print(self.leftview)
+        print(self.rightview)
+        #print(self.midview)
 
-    def moveStaright(self):
-        print('move straight')
-        # error = ...
-        # while(abs(error) < value):
+    def moveStraight(self):
+        target_distance = 0.7
+        error = target_distance - self.midview
 
-    def turn(self, sens):
+        while (abs(error)>0.01):
+            error = target_distance - self.midview
+            print(error)
+            self.cmd.linear.x = self.kP*abs(error)
+            self.pub_cmd.publish(self.cmd)
+            print('esquerda: {}' .format(self.leftview))
+            print('direita: {}'.format(self.rightview))
+        self.move_error = error
+
+    def error(self,target):
+        error = target - self.yaw
+        print('erro: {}'.format(error))
+        print('angulo atual: {}'.format(self.yaw))
+        return error
+
+    def turn(self, sensor, target):
         print('turn')
-        target_angle = 90
-        target_rad = target_angle * math.pi/180
-        while(abs(error) < value):
-            error = target_rad - self.yaw
-            cmd.angular.z = self.kP * error
-            self.base_pub.publish(cmd)
-
-    def decision(self):
-        print('decision')
-        #
+        while (abs(self.error(target)) > 0.01):
+            self.cmd.angular.z = sensor*self.kP*self.error(target)
+            self.pub_cmd.publish(self.cmd)
+        self.cmd.angular.z = 0
+        self.pub_cmd.publish(self.cmd)
 
 if __name__ == '__main__':
+    # Define the node
+    rospy.init_node('TIAGo_odem_node')
 
-    rospy.init_node('nodeName')
+    # Create an object of class mySub and run the init function
+    subObj = myRobot()
 
-    tiago = myRobot()
+    if subObj.leftview < 1.3 and subObj.rightview < 1.3:
+        subObj.moveStraight()
+    if subObj.move_error < 0.01:
+        if subObj.leftview < 1.3:
+            target = -90 * math.pi / 180 + subObj.yaw
+            subObj.error(target)
+            sensor = 1
+            subObj.turn(sensor,target)
+            subObj.moveStraight()
+        elif subObj.rightview < 1.3:
+            target = 90 * math.pi / 180 + subObj.yaw
+            subObj.error(target)
+            sensor = -1
+            subObj.turn(sensor,target)
+            subObj.moveStraight()
+    if subObj.leftview > 1.3 or subObj.rightview > 1.3:
+        subObj.cmd_head.joint_names.append("head_1_joint")
+        subObj.cmd_head.joint_names.append("head_2_joint")
+        subObj.points.positions = [0] * 2
+        subObj.points.time_from_start = rospy.Duration(1)
+        subObj.cmd_head.points.append(subObj.points)
+        angle = 0.1
+        while angle < 2:
+            subObj.cmd_head.points[0].positions[0] = angle
+            subObj.cmd_head.points[0].time_from_start = rospy.Duration(1)
+            subObj.pub_head.publish(subObj.cmd_head)
+            angle = angle + 0.01
+            print(angle)
+        print('camera')
+        rospy.sleep(3)
+       # rospy.init_node('addImage_service_name')
+        subCam = myCamera()
+        my_service = rospy.Service('addImage_service_name', addImage, subCam.callback_ServiceCamera)
+        if subCam.resposta == 'green':
+            target = 90 * math.pi / 180 + subObj.yaw
+            subObj.error(target)
+            sensor = -1
+            subObj.turn(sensor, target)
+            subObj.moveStraight()
+        elif subCam.resposta == 'red':
+	    target = -90 * math.pi / 180 + subObj.yaw
+            subObj.error(target)
+            sensor = 1
+            subObj.turn(sensor,target)
+            subObj.moveStraight()
 
-    state = 0
-    # while(...):
-     # if state == 0:
-        # decision
-        # compute next state
-     # else if state == 1
-        # image porcessing
-        # compute next state
-     # else if state == 3
-        # move straight
-        # compute next state
-     # else if state == 4
-        # turn
-        # compute next state
+
+           
+    # While ROS is running
+    rospy.spin()
